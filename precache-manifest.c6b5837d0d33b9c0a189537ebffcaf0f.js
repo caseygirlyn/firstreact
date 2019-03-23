fcaf64d182d5dfaa585d3bd29ce86fd2042cefb1
Girlyn Antillon <antillon.girlyn@gmail.com>
2019-03-23 15:01:30 +0000

    Updates

---
+++ precache-manifest.c6b5837d0d33b9c0a189537ebffcaf0f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "174536efe32f1ef23c69",
+    "url": "/firstreact/static/css/main.eb6a06d3.chunk.css"
+  },
+  {
+    "revision": "174536efe32f1ef23c69",
+    "url": "/firstreact/static/js/main.4253a718.chunk.js"
+  },
+  {
+    "revision": "123392da67e38e09f938",
+    "url": "/firstreact/static/js/runtime~main.a7d87b6e.js"
+  },
+  {
+    "revision": "2509082a323abbd7c81a",
+    "url": "/firstreact/static/js/2.1c7adc82.chunk.js"
+  },
+  {
+    "revision": "33d1ba09bd28fdeb8569f355497acc0d",
+    "url": "/firstreact/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/caseygirlyn/firstreact/precache-manifest.634c34f1f53713a1768c527d01de9f5d.js"
+  "/firstreact/precache-manifest.c6b5837d0d33b9c0a189537ebffcaf0f.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/caseygirlyn/firstreact/index.html", {
+workbox.routing.registerNavigationRoute("/firstreact/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
